c824c0f456e54dbb885e538a2064cf216886f620
Dmitriy G. Golub <d.golub@hostco.ru>
2020-01-28 13:48:16 +0500

    Graphviz inclusion doesn't work after Pandoc upgrade, I gave up.

--- make_html.sh
+++ make_html.sh
@@ -2,10 +2,11 @@
 pp thinking_a8.map |
 /usr/bin/pandoc -f markdown -t html5 \
    -o /tmp/think.html \
-   -F ~/bin/pandoc-filter-graphviz \
    --self-contained \
    --metadata title="Кейс A8" \
    --toc \
    -
 # cleanup: remove temporary files
 ls -1 | ag 'graphviz-[0-9a-f]{64}.png' | xargs rm
+
+#   -F ~/bin/pandoc-filter-graphviz \
